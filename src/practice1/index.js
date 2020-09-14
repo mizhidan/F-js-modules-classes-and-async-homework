e65eb6ad83e790c51ba13fcd3b2f22c679e0ba09
mizhidan <dj9824@126.com>
2020-09-14 17:24:56 +0800

    [danjie] feat: practice 1

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,8 @@
+import getUrl from "./utils";
+import { ADDRESS, PATH, PORT, REGION } from "./constant";
 
+const url = getUrl(ADDRESS, PORT, PATH);
+
+const region = REGION;
+
+export { url, region };
--- src/practice1/utils.js
+++ src/practice1/utils.js
@@ -1,1 +1,6 @@
-const getURL
+import { get } from "fetch-mock"
+const getURL = (address, port, path) => {
+  return `${address}:${port}${path}`;
+};
+
+export default getURL;
