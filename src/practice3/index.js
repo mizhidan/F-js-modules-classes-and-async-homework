57c366d23b75ea355459792fa5082cd8eebd1db4
mizhidan <dj9824@126.com>
2020-09-18 12:02:56 +0800

    [danjie] feat: exercise 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,8 @@
-export const getPoetry
+export const getPoetry = async () => {
+  let result;
+  await fetch("https://v1.jinrishici.com/all.json")
+    .then((resp) => resp.json())
+    .then((data) => (result = [data.origin, data.author, data.content]))
+    .catch((err) => console.log(err));
+  return result;
+};
